f223747d7cdf5c2887b2785d0d69b0a45e50b5d7
22MH1A05K5 <119723590+22MH1A05K5@users.noreply.github.com>
2022-12-10 11:00:33 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         int x,rem,r=0;         scanf("%d",&x);         int tp=x;         while(x!=0)         {             rem=x%10;             r=r*10+rem;             x=x/10;         }         if(tp==r)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        int x,rem,r=0;
+        scanf("%d",&x);
+        int tp=x;
+        while(x!=0)
+        {
+            rem=x%10;
+            r=r*10+rem;
+            x=x/10;
+        }
+        if(tp==r)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    }
+}
