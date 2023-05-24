7f1e042be0b6ec17196e50d6247c5b023fd36123
22MH1A05K5 <119723590+22MH1A05K5@users.noreply.github.com>
2023-05-24 17:12:15 +0530

    #include <stdio.h> int isHappyNumber(int num){     int rem = 0, sum = 0;     while(num > 0){         rem = num%10;         sum = sum + (rem*rem);         num = num/10;     }     return sum; } int main() {     int num;     scanf("%d",&num);     int result = num;     while(result != 1 && result != 4) 	{         result = isHappyNumber(result);     }     if(result == 1)         printf("True");     else if(result == 4)         printf("False");      return 0; }

--- happy_number.c
+++ happy_number.c
@@ -1,21 +1,25 @@
-#include<stdio.h>
-int main()
-{
-    int i,j,n,temp,sum=0;
-    scanf("%d",&n);
-    while(sum!=1&&sum!=4)
-    {
-        sum=0;
-        while(n>0)
-        {
-            j=n%10;
-            sum+=(j*j);
-            n=n/10;
-        }
-        n=sum;
+#include <stdio.h> 
+int isHappyNumber(int num){    
+    int rem = 0, sum = 0;    
+    while(num > 0){    
+        rem = num%10;    
+        sum = sum + (rem*rem);    
+        num = num/10;    
+    }    
+    return sum;    
+}     
+int main()    
+{    
+    int num;
+    scanf("%d",&num);
+    int result = num;    
+    while(result != 1 && result != 4)
+	{    
+        result = isHappyNumber(result);    
     }
-    if(sum==1)
-    printf("True");
-    else
-    printf("False");
-}
+    if(result == 1)    
+        printf("True");      
+    else if(result == 4)    
+        printf("False");     
+     return 0;    
+}    
